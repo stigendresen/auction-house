01fdd2e5e4823506416cc6763c6e912d58f2b64a
hsirvio <heinosirvio@gmail.com>
2013-12-01 12:33:28 +0200

    Added rest-framework to settings and urls

--- Auction/settings.py
+++ Auction/settings.py
@@ -95,3 +95,16 @@ STATICFILES_DIRS = (os.path.join(os.path.dirname(__file__), '..', 'static').repl
 STATIC_URL = '/static/'
 
 ANALYTICS_ID = "UA-XXXXXXX-X"
+
+REST_FRAMEWORK = {
+    # Use hyperlinked styles by default.
+    # Only used if the `serializer_class` attribute is not set on a view.
+    'DEFAULT_MODEL_SERIALIZER_CLASS':
+        'rest_framework.serializers.HyperlinkedModelSerializer',
+
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
